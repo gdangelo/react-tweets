f3b1eb630ea776cdff86cc4ee8197654ff61a0e0
gdangelo <dangelo.gregory@gmail.com>
2015-09-01 08:42:07 +0200

    Add a config example file

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+    twitter : {
+      consumer_key: 'foo', //'TWITTER_CONSUMER_KEY',
+      consumer_secret: 'bar', //'TWITTER_CONSUMER_SECRET',
+      access_token: 'toto', //'TWITTER_ACCESS_TOKEN_KEY',
+      access_token_secret: 'titi' //'TWITTER_ACCESS_TOKEN_SECRET'
+    }
+}
--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    twitter : {
-      consumer_key: 'VMHHyTIGbcMbCXeRDrwjX5jHN', //'TWITTER_CONSUMER_KEY',
-      consumer_secret: '4pRQ36xm3vEdBIjGWMl01ReUfMwPt7Y2tCUeybOG6QP8Awu8F9', //'TWITTER_CONSUMER_SECRET',
-      access_token: '1196844678-vbTyG1Vh8xCArxNxS00iANW4jPefNS7uEMhJf28', //'TWITTER_ACCESS_TOKEN_KEY',
-      access_token_secret: 'Eni4XcYMGeQk1mbROyXYW3tfsmnUqExkt8HSlPCjedwGn' //'TWITTER_ACCESS_TOKEN_SECRET'
-    }
-}
