aa3293f6afef37386a35242bb307f34731422457
gdangelo <dangelo.gregory@gmail.com>
2015-09-01 21:44:57 +0200

    Create 'Loader' React component

---
+++ components/Loader.react.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Loader = React.createClass({
+
+    render: function () {
+
+      return (
+        <div className={"loader" + (this.props.paging ? ' active' : '')}>
+          <img src="svg/loader.svg" />
+        </div>
+      );
+    }
+
+});
+
+module.exports = Loader;
