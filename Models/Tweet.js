14fd037cf8dd76e7f912564c5c9f0ffff8ba732b
gdangelo <dangelo.gregory@gmail.com>
2015-09-02 22:12:58 +0200

    Connect to MongoDB using mongoose

---
+++ Models/Tweet.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+// Create a new schema for the tweet data
+var schema = new Schema({
+    twid       : String
+  , active     : Boolean
+  , author     : String
+  , avatar     : String
+  , body       : String
+  , date       : Date
+  , screenname : String
+});
+
+// Create a static method to return tweet data from the mongodb
+schema.static.getTweets = function (page, skip, callback) {
+  var tweets = [],
+      start = (page * 10) + (skip * 1);
+
+  // Query the db, using skip and limit to achieve page chunks
+  this.find({}, 'twid active author avatar body date screenname', {skip: start, limit: 10}).sort({date: 'desc'}).exec(function(err,docs)) {
+
+    if(!err) {
+      tweets = docs;
+      tweets.forEach(function (tweet) {
+        tweet.active = true; // Set retrieved tweets to active
+      });
+    }
+
+    // Pass them back to the specified callback
+    callback(tweets);
+
+  };
+};
+
+// Return a Tweet model based upon the defined schema
+module.exports = Tweet = mongoose.model('Tweet', schema);
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// Require our dependencies
 var express = require('express'),
     exphbs = require('express-handlebars'),
     http = require('http'),
-    path = require('path');
+    path = require('path'),
+    mongoose = require('mongoose');
 
 // Create an express instance and set a host and port variables
 var app = express();
@@ -12,10 +14,15 @@ var host = process.env.HOST || 'localhost';
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Connect to the mongo database
+mongoose.connect('mongodb://localhost/react-tweets');
+
+// Index Route
 app.get('/', function (req, res) {
   res.render('home');
 });
 
+// Start the server
 var server = http.createServer(app);
 server.listen(port, host, function () {
   console.log('Express server listening at %s:%d, within %s environment.', host, port, app.get('env'));
